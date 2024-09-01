fa1ad5fd1e9e85baa6b6b69e058161c42c564b62
david-o-rosario1201 <finnnn023@gmail.com>
2024-08-31 21:00:57 -0400

    Creación de la base de datos PrioridadDb e implementación del archivo dao PrioridadDao

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/local/database/PrioridadDb.kt
@@ -0,0 +1,14 @@
+package edu.ucne.composeregistroprioridadesap2.data.local.database
+
+import androidx.room.Database
+import edu.ucne.composeregistroprioridadesap2.data.local.dao.PrioridadDao
+import edu.ucne.composeregistroprioridadesap2.data.local.entities.PrioridadEntity
+
+@Database(
+    version = 1,
+    exportSchema = false,
+    entities = [PrioridadEntity::class]
+)
+abstract class PrioridadDb {
+    abstract fun prioridadDao(): PrioridadDao
+}
