da10762ccd8f91145c07e21ab3234a3ccde8ef26
david-o-rosario1201 <finnnn023@gmail.com>
2024-08-31 21:04:42 -0400

    Declaración y construcción de la base de datos en el archivo MainActivity.

--- app/src/main/java/edu/ucne/composeregistroprioridadesap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/MainActivity.kt
@@ -11,12 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import edu.ucne.composeregistroprioridadesap2.data.local.database.PrioridadDb
 import edu.ucne.composeregistroprioridadesap2.ui.theme.RegistroPrioridadesAp2Theme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var prioridadDb: PrioridadDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        prioridadDb = Room.databaseBuilder(
+            applicationContext,
+            PrioridadDb::class.java,
+            "Prioridad.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
         setContent {
             RegistroPrioridadesAp2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -28,20 +40,20 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroPrioridadesAp2Theme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        RegistroPrioridadesAp2Theme {
+            Greeting("Android")
+        }
     }
 }
