6bf0763222eed1bd5d9a52011449461a8de3aeb2
david-o-rosario1201 <finnnn023@gmail.com>
2024-08-31 21:00:05 -0400

    Creación del archivo dao PrioridadDao.

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/local/dao/PrioridadDao.kt
@@ -0,0 +1,29 @@
+package edu.ucne.composeregistroprioridadesap2.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.composeregistroprioridadesap2.data.local.entities.PrioridadEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PrioridadDao {
+    @Upsert()
+    suspend fun save(prioridad: PrioridadEntity)
+
+    @Query(
+        """
+            SELECT * FROM Prioridades
+            WHERE prioridadId = :id
+            LIMIT 1
+        """
+    )
+    suspend fun find(id: Int): PrioridadEntity?
+
+    @Delete
+    suspend fun delete(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM PRIORIDADES")
+    fun getAll(): Flow<List<PrioridadEntity>>
+}
