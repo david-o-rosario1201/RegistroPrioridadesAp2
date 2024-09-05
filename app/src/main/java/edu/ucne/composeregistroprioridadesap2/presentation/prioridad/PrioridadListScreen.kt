42e1b77a43f5e69ab8e12b21876eaed7617c27a6
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-05 15:13:56 -0400

    Creación del archivo PrioridadListScreen

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/presentation/prioridad/PrioridadListScreen.kt
@@ -0,0 +1,126 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package edu.ucne.composeregistroprioridadesap2.presentation.prioridad
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import edu.ucne.composeregistroprioridadesap2.data.local.entities.PrioridadEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun PrioridadListScreen(
+    scope: CoroutineScope,
+    prioridadList: List<PrioridadEntity>,
+    onAddPrioridad: () -> Unit
+){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(text = "Prioridades")
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddPrioridad
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Agregar nueva prioridad"
+                )
+            }
+        }
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(it)
+        ){
+            Spacer(modifier = Modifier.height(32.dp))
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ){
+                items(prioridadList){
+                    PrioridadRow(
+                        it = it,
+                        scope = scope
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PrioridadRow(
+    scope: CoroutineScope,
+    it: PrioridadEntity
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = it.prioridadId.toString(),
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = it.descripcion,
+            modifier = Modifier.weight(2f)
+        )
+        Text(
+            text = it.diasCompromiso.toString(),
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(
+            onClick = {
+                scope.launch {
+//                    deletePrioridad(
+//                        PrioridadEntity(
+//                            prioridadId = it.prioridadId,
+//                            descripcion = it.descripcion,
+//                            diasCompromiso = it.diasCompromiso
+//                        )
+//                    )
+                }
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Eliminar prioridad"
+            )
+        }
+    }
+    HorizontalDivider()
+}
