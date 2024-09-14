3bc09c2820095f82aee6baf2b34c3d282ec01483
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-14 19:04:24 -0400

    Se eliminó la implementación de la base de datos del parámetro de la función. Se eliminó también el scope y la llama para obtener la lista a través de la implementación de la base de datos y se le cambió el nombre de esta función a RegistroPrioridadesAp2NavHost

--- app/src/main/java/edu/ucne/composeregistroprioridadesap2/presentation/navigation/RegistroPrioridadesAp2NavHost.kt
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/presentation/navigation/RegistroPrioridadesAp2NavHost.kt
@@ -1,48 +1,28 @@
 package edu.ucne.composeregistroprioridadesap2.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import edu.ucne.composeregistroprioridadesap2.data.local.database.PrioridadDb
 import edu.ucne.composeregistroprioridadesap2.presentation.prioridad.PrioridadListScreen
 import edu.ucne.composeregistroprioridadesap2.presentation.prioridad.PrioridadScreen
 
 @Composable
-fun RegistroPrioridadesAp2(
-    prioridadDb: PrioridadDb,
+fun RegistroPrioridadesAp2NavHost(
     navHostController: NavHostController
 ){
-    val scope = rememberCoroutineScope()
-
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val prioridadList by prioridadDb.prioridadDao().getAll()
-        .collectAsStateWithLifecycle(
-            initialValue = emptyList(),
-            lifecycleOwner = lifecycleOwner,
-            minActiveState = Lifecycle.State.STARTED
-        )
-
     NavHost(
         navController = navHostController,
         startDestination = Screen.PrioridadListScreen
     ) {
         composable<Screen.PrioridadListScreen> {
             PrioridadListScreen(
-                prioridadDb = prioridadDb,
-                prioridadList = prioridadList,
                 onAddPrioridad = {
                     navHostController.navigate(Screen.PrioridadScreen(0))
                 },
-                scope = scope,
-                onPrioridadClick = { prioridad ->
-                    navHostController.navigate(Screen.PrioridadScreen(prioridad.prioridadId ?: 0))
+                onPrioridadClick = { prioridadId ->
+                    navHostController.navigate(Screen.PrioridadScreen(prioridadId = prioridadId))
                 }
             )
         }
@@ -50,9 +30,7 @@ fun RegistroPrioridadesAp2(
             val id = argumento.toRoute<Screen.PrioridadScreen>().prioridadId
 
             PrioridadScreen(
-                id = id,
-                prioridadDb = prioridadDb,
-                scope = scope,
+                prioridadId = id,
                 goPrioridadList = {
                     navHostController.navigate(
                         Screen.PrioridadListScreen
