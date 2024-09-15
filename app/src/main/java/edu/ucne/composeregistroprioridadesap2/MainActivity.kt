79b558dad60a74515f337321f2ccb050b0f80e51
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-15 11:34:39 -0400

    Se agregó la función BuildNavigationItems para construir los items que va a tener el drawer

--- app/src/main/java/edu/ucne/composeregistroprioridadesap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.outlined.Warning
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.composeregistroprioridadesap2.presentation.navigation.RegistroPrioridadesAp2NavHost
@@ -17,10 +24,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             RegistroPrioridadesAp2Theme {
                 val navController = rememberNavController()
+                val items = BuildNavigationItems(0)
+
                 RegistroPrioridadesAp2NavHost(
-                    navHostController = navController
+                    navHostController = navController,
+                    items = items
                 )
             }
         }
     }
+}
+
+fun BuildNavigationItems(taskCount: Int): List<NavigationItem>{
+    return listOf(
+        NavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            route = Route.HOME
+        ),
+        NavigationItem(
+            title = "Prioridades",
+            selectedIcon = Icons.Filled.Warning,
+            unselectedIcon = Icons.Outlined.Warning,
+            route = Route.PRIORIDAD
+        ),
+        NavigationItem(
+            title = "Tickets",
+            selectedIcon = Icons.Filled.Star,
+            unselectedIcon = Icons.Outlined.Star,
+            route = Route.TICKET
+        )
+    )
+}
+
+enum class Route{
+    HOME,
+    PRIORIDAD,
+    TICKET
 }
