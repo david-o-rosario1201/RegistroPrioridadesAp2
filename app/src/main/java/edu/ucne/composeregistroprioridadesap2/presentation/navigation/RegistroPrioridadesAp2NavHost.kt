ffd8b181cb0695862b44b5430bfa5fd9df83c38d
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-15 11:39:53 -0400

    Se agregó un drawer para manejar el menú, además de las nuevas ventanas TicketScreen, TicketListScreen y HomeScreen

--- app/src/main/java/edu/ucne/composeregistroprioridadesap2/presentation/navigation/RegistroPrioridadesAp2NavHost.kt
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/presentation/navigation/RegistroPrioridadesAp2NavHost.kt
@@ -1,42 +1,136 @@
 package edu.ucne.composeregistroprioridadesap2.presentation.navigation
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import edu.ucne.composeregistroprioridadesap2.NavigationItem
+import edu.ucne.composeregistroprioridadesap2.Route
+import edu.ucne.composeregistroprioridadesap2.presentation.home.HomeScreen
 import edu.ucne.composeregistroprioridadesap2.presentation.prioridad.PrioridadListScreen
 import edu.ucne.composeregistroprioridadesap2.presentation.prioridad.PrioridadScreen
+import edu.ucne.composeregistroprioridadesap2.presentation.ticket.TicketListScreen
+import edu.ucne.composeregistroprioridadesap2.presentation.ticket.TicketScreen
+import kotlinx.coroutines.launch
+
 
 @Composable
 fun RegistroPrioridadesAp2NavHost(
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    items: List<NavigationItem>
 ){
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.PrioridadListScreen
-    ) {
-        composable<Screen.PrioridadListScreen> {
-            PrioridadListScreen(
-                onAddPrioridad = {
-                    navHostController.navigate(Screen.PrioridadScreen(0))
-                },
-                onPrioridadClick = { prioridadId ->
-                    navHostController.navigate(Screen.PrioridadScreen(prioridadId = prioridadId))
-                }
-            )
-        }
-        composable<Screen.PrioridadScreen> { argumento ->
-            val id = argumento.toRoute<Screen.PrioridadScreen>().prioridadId
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(
+                    text = "  Menu",
+                    style = MaterialTheme.typography.displaySmall,
+                    fontWeight = FontWeight.Bold
+                )
 
-            PrioridadScreen(
-                prioridadId = id,
-                goPrioridadList = {
-                    navHostController.navigate(
-                        Screen.PrioridadListScreen
+                Spacer(modifier = Modifier.height(16.dp))
+                items.forEachIndexed { index, item ->
+                    NavigationDrawerItem(
+                        label = {
+                            Text(item.title)
+                        },
+                        selected = index == selectedItemIndex,
+                        onClick = {
+                            if(item.route == Route.HOME)
+                                navHostController.navigate(Screen.HomeScreen)
+                            if(item.route == Route.PRIORIDAD)
+                                navHostController.navigate(Screen.PrioridadListScreen)
+                            else if(item.route == Route.TICKET)
+                                navHostController.navigate(Screen.TicketListScreen)
+                            selectedItemIndex = index
+                            scope.launch {drawerState.close()}
+                        }
                     )
                 }
-            )
+            }
+        },
+        drawerState = drawerState
+    ) {
+        NavHost(
+            navController = navHostController,
+            startDestination = Screen.HomeScreen
+        ) {
+
+            composable<Screen.HomeScreen>{
+                HomeScreen(
+                    drawerState = drawerState,
+                    scope = scope,
+                    navHostController = navHostController
+                )
+            }
+
+            composable<Screen.PrioridadListScreen> {
+                PrioridadListScreen(
+                    onAddPrioridad = {
+                        navHostController.navigate(Screen.PrioridadScreen(0))
+                    },
+                    onPrioridadClick = { prioridadId ->
+                        navHostController.navigate(Screen.PrioridadScreen(prioridadId = prioridadId))
+                    }
+                )
+            }
+            composable<Screen.PrioridadScreen> { argumento ->
+                val id = argumento.toRoute<Screen.PrioridadScreen>().prioridadId
+
+                PrioridadScreen(
+                    prioridadId = id,
+                    goPrioridadList = {
+                        navHostController.navigate(
+                            Screen.PrioridadListScreen
+                        )
+                    }
+                )
+            }
+            composable<Screen.TicketListScreen> {
+                TicketListScreen(
+                    drawerState = drawerState,
+                    scope = scope,
+                    onAddTicket = {
+                        navHostController.navigate(Screen.TicketScreen( 0))
+                    },
+                    onTicketClick = { ticketId ->
+                        navHostController.navigate(Screen.TicketScreen(ticketId = ticketId))
+                    }
+                )
+            }
+            composable<Screen.TicketScreen> {argumento ->
+                val ticketId = argumento.toRoute<Screen.TicketScreen>().ticketId
+
+                TicketScreen(
+                    ticketId = ticketId,
+                    goTicketList = {
+                        navHostController.navigate(
+                            Screen.TicketListScreen
+                        )
+                    }
+                )
+            }
         }
     }
 }
