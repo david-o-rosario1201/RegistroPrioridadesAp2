6219597dafb4ea4caf9acaae828472bf288cf392
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-06 23:58:47 -0400

    Creación de la interfaz SistemasApi

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/remote/SistemasApi.kt
@@ -0,0 +1,18 @@
+package edu.ucne.composeregistroprioridadesap2.data.remote
+
+import edu.ucne.composeregistroprioridadesap2.data.remote.dto.SistemaDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface SistemasApi {
+    @GET("api/Sistemas/{sistemaId}")
+    suspend fun getSistema(@Path("sistemaId") sistemaId: Int): SistemaDto
+
+    @GET("api/Sistemas")
+    suspend fun getAll(): List<SistemaDto>
+
+    @POST("api/Sistemas")
+    suspend fun save(@Body sistemaDto: SistemaDto?): SistemaDto
+}
