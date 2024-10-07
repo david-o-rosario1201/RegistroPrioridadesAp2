d32df0c960b647a2b7d54066b8acd5cb4f68827d
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-07 00:04:06 -0400

    Creación de la clase ClienteRepository

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/repository/ClienteRepository.kt
@@ -0,0 +1,15 @@
+package edu.ucne.composeregistroprioridadesap2.data.repository
+
+import edu.ucne.composeregistroprioridadesap2.data.remote.ClientesApi
+import edu.ucne.composeregistroprioridadesap2.data.remote.dto.ClienteDto
+import javax.inject.Inject
+
+class ClienteRepository @Inject constructor(
+    private val clienteApi: ClientesApi
+){
+    suspend fun getClienteById(clienteId: Int) = clienteApi.getCliente(clienteId)
+
+    suspend fun save(clienteDto: ClienteDto) = clienteApi.save(clienteDto)
+
+    suspend fun getAll() = clienteApi.getAll()
+}
