9c84f1a4c802e68c39293749f6e8d2adb3b3f9dd
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-07 00:03:32 -0400

    Creación de ClienteDto, ClientesDetalleCelulares y ClientesDetalleTelefonos

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/remote/dto/ClientesDetalleCelulares.kt
@@ -0,0 +1,7 @@
+package edu.ucne.composeregistroprioridadesap2.data.remote.dto
+
+data class ClientesDetalleCelulares(
+    val id: Int?,
+    val clienteId: Int,
+    val celular: String
+)
---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/remote/dto/ClientesDetalleTelefonos.kt
@@ -0,0 +1,7 @@
+package edu.ucne.composeregistroprioridadesap2.data.remote.dto
+
+data class ClientesDetalleTelefonos(
+    val id: Int?,
+    val clienteId: Int,
+    val telefono: String
+)
