39b9ea5c43d5e521003d5df4994f82ef0b9f889c
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-06 23:59:35 -0400

    Creación de la clase SistemaRepository

---
+++ app/src/main/java/edu/ucne/composeregistroprioridadesap2/data/repository/SistemaRepository.kt
@@ -0,0 +1,15 @@
+package edu.ucne.composeregistroprioridadesap2.data.repository
+
+import edu.ucne.composeregistroprioridadesap2.data.remote.SistemasApi
+import edu.ucne.composeregistroprioridadesap2.data.remote.dto.SistemaDto
+import javax.inject.Inject
+
+class SistemaRepository @Inject constructor(
+    private val sistemaApi: SistemasApi
+){
+    suspend fun getSistemaById(sistemaId: Int) = sistemaApi.getSistema(sistemaId)
+
+    suspend fun save(sistemaDto: SistemaDto) = sistemaApi.save(sistemaDto)
+
+    suspend fun getAll() = sistemaApi.getAll()
+}
